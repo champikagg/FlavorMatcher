2c28a688c9ae981f3211843f437b8cc14936206c
chamathla <chamathla@yahoo.com>
2014-07-15 11:03:36 -0400

    templates for the app

--- Rest_Farmington/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from Rest_Farmington.models import Rest_Farmington_Hills
-
-# Register your models here.
-admin.site.register(Rest_Farmington_Hills)
--- Rest_Farmington/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from Rest_Farmington.models import Rest_Farmington_Hills
-
-class RestForm(forms.Form):
-	restaurant = forms.CharField(max_length=50)
-	zipcode = forms.CharField(max_length=50)
-	miles=forms.CharField(max_length=10)
